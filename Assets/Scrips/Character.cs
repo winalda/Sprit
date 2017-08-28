9e7bc53099c8f9326fdccbf262178fcc548a0000
winalda <alberto@kokonutstudio.com>
2017-08-27 22:52:10 -0500

    Spritman

---
+++ Assets/Scrips/Character.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	public GameObject newSpritman;
+	public GameObject stopSpritman;
+	public GameObject RunSpritman;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (newSpritman, new Vector3 (-9f, -3.5f, 0), new Quaternion(0,0,0,0));//newSpritman.transform.rotation);
+		this.CharacterGame();
+	}
+
+	void CharacterGame(){
+		System.Threading.Thread.Sleep (10000000);
+		//DestroyObject (newSpritman);
+		newSpritman.SetActive(false);
+	}
+
+	// Update is called once per 	frame
+	//void Update () {
+	//}
+}
