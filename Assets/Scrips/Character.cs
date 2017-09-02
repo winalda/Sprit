a223723e7351f9d94b5bb4f87cb5ceaa8c249c9d
winalda <alberto@kokonutstudio.com>
2017-09-01 20:20:03 -0500

    avance en el juego y interaccion con las animaciones

--- Assets/Scrips/Character.cs
+++ Assets/Scrips/Character.cs
@@ -10,14 +10,14 @@ public class Character : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Instantiate (newSpritman, new Vector3 (-9f, -3.5f, 0), new Quaternion(0,0,0,0));//newSpritman.transform.rotation);
+		Instantiate (newSpritman, new Vector3 (-9f, -3.5f, 0), Quaternion.identity);//new Quaternion(0,0,0,0));//newSpritman.transform.rotation);
 		this.CharacterGame();
 	}
 
 	void CharacterGame(){
-		System.Threading.Thread.Sleep (10000000);
+		//System.Threading.Thread.Sleep (10000000);
 		//DestroyObject (newSpritman);
-		newSpritman.SetActive(false);
+		//newSpritman.SetActive(false);
 	}
 
 	// Update is called once per 	frame
